d584080963a56bc1e20bf585deab038cca267435
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2024-01-30 14:43:03 +0530

    Strivers' CP Sheet Start #1

---
+++ strivers_cp/implementation/1_bit++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    int res = 0;
+    string op;
+    for(int i=0; i<n; i++){
+        cin >> op;
+        if(op == "++X" || op == "X++")
+            res++;
+        else    
+            res--;
+    }
+
+    cout << res;
+    return 0;
+} 
